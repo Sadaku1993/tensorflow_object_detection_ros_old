7f5f10b926a95815bbe7bc1cff89eb47b46a1a1f
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-08-05 21:51:27 +0900

    update

--- object_detection/object_detection_ros.py
+++ object_detection/object_detection_ros.py
@@ -20,6 +20,8 @@ from utils import visualization_utils as vis_util
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+from amsl_recog_msgs.msg import ObjectInfoWithROI
+from amsl_recog_msgs.msg import ObjectInfoArray
 
 # MODEL_NAME = 'ssd_mobilenet_v1_coco_2017_11_17'
 # MODEL_NAME = 'ssdlite_mobilenet_v2_coco_2018_05_09'
@@ -56,6 +58,7 @@ class ObjectDetection(object):
     def __init__(self, detection_graph, category_index):
         self.image_sub = rospy.Subscriber("/image", Image, self.imageCallback, queue_size=10)
         self.image_pub = rospy.Publisher("/object_detection/image", Image, queue_size=10)
+        self.bbox_pub  = rospy.Publisher("/objectinfo", ObjectInfoArray, queue_size=10)
         self.detection_graph = detection_graph
         self.category_index  = category_index
         self.flag = False
@@ -75,6 +78,46 @@ class ObjectDetection(object):
                     allow_growth=True
                     )
                 )
+
+    def publish_boxes_and_labels(
+        self,
+        image,
+        boxes,
+        classes,
+        scores,
+        category_index,
+        max_boxes_to_draw=20,
+        min_score_thresh=.4):
+
+        bbox_array = ObjectInfoArray()
+        if not max_boxes_to_draw:
+            max_boxes_to_draw = boxes.shape[0]
+        for i in range(min(max_boxes_to_draw, boxes.shape[0])):
+            if scores is None or scores[i] > min_score_thresh:
+                if classes[i] in category_index.keys():
+                    class_name = category_index[classes[i]]['name']
+                else:
+                    class_name = 'N/A'
+                box = tuple(boxes[i].tolist())
+                label = classes[i]
+                score = scores[i]
+                ymin, xmin, ymax, xmax = box
+                im_height = image.shape[0]
+                im_width  = image.shape[1]
+                (left, right, top, dowm) = (xmin * im_width, xmax * im_width,
+                                            ymin * im_height, ymax * im_height)
+                bbox = ObjectInfoWithROI()
+                bbox.Class = class_name
+                bbox.probability = score
+                bbox.xmin = left
+                bbox.xmax = right
+                bbox.ymin = top
+                bbox.ymax = dowm
+                bbox_array.object_array.append(bbox)
+                print('%s:%.2f left:%d right:%d top:%d dowm:%d' % (class_name, score, left, right, top, dowm))
+        bbox_array.header.stamp = rospy.Time.now()
+        self.bbox_pub.publish(bbox_array)
+
     def object_detection(self, sess):
         image_np = self.cv_image
         image_np_expanded = np.expand_dims(image_np, axis=0)
@@ -87,6 +130,15 @@ class ObjectDetection(object):
         (boxes, scores, classes, num_detections) = sess.run(
                 [boxes, scores, classes, num_detections],
                 feed_dict={image_tensor: image_np_expanded})
+
+        # Publish object info
+        self.publish_boxes_and_labels(
+                self.cv_image,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                self.category_index)
+
         # Visualization of the results of a detection.
         vis_util.visualize_boxes_and_labels_on_image_array(
                 image_np,
@@ -95,7 +147,8 @@ class ObjectDetection(object):
                 np.squeeze(scores),
                 self.category_index,
                 use_normalized_coordinates=True,
-                line_thickness=8)
+                line_thickness=4,
+                min_score_thresh=.3)
 
         image_height = self.cv_image.shape[0]
         image_width  = self.cv_image.shape[1]
